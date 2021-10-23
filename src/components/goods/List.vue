c1a48b7e60c5a6775ace60f2a2fec4e33e34cbcc
shelley326 <1993682155@qq.com>
2021-10-23 20:54:40 +0800

    更新分类模块

---
+++ src/components/goods/List.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">商品列表</div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {}
+  },
+}
+</script>
+
+<style scoped lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import Users from "../components/user/Users.vue";
 import Rights from "../components/power/Rights.vue";
 import Roles from "../components/power/Roles.vue";
 import Cate from "../components/goods/Cate.vue";
+import Params from "../components/goods/Params.vue";
+import List from "../components/goods/List.vue";
 
 Vue.use(VueRouter);
 
@@ -40,6 +42,14 @@ const routes = [
       {
         path: "/categories",
         component: Cate
+      },
+      {
+        path: "/params",
+        component: Params
+      },
+      {
+        path: "/goods",
+        component: List
       },
     ],
   },
